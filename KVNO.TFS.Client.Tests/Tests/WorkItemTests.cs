4ba4071f2a073fdf198f93ee5c4e1e2b227c6ee3
Ben Richter <50136177+Benexdrake@users.noreply.github.com>
2023-03-20 20:29:16 +0100

    added a Workitem Test to check if service works

---
+++ KVNO.TFS.Client.Tests/Tests/WorkItemTests.cs
@@ -0,0 +1,32 @@
+﻿using KVNO.TFS.Client.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace KVNO.TFS.Client.Tests;
+
+public class WorkItemTests
+{
+    private readonly CollectionService _cs;
+    private readonly ProjectService _ps;
+    private readonly WorkItemService _ws;
+    public WorkItemTests()
+    {
+        var sp = new DI().Start().Result;
+
+        _cs = sp.GetRequiredService<CollectionService>();  
+        _ps = sp.GetRequiredService<ProjectService>();
+        _ws = sp.GetRequiredService<WorkItemService>();
+    }
+
+    [Fact]
+    public async Task GetWorkitems()
+    {
+        var collections = await _cs.GetCollections();
+        var collection1 = collections.FirstOrDefault();
+        var projects = await _ps.GetProjects(collection1.Name, collection1.Id);
+        var project1 = projects.FirstOrDefault();
+        var workitems = await _ws.GetWorkItems(collection1.Name, project1.Name, project1.Id, "Task");
+
+        Assert.InRange(workitems.Length, 1, int.MaxValue);
+    }
+}
